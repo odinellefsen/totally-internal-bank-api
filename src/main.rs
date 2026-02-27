b1e0d4cfc4920fe1b9f5e95611df71b26881a2cb
odinellefsen <odin@ellefsen.fo>
2026-02-27 23:28:43 +0000

    add non working hello world actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World");
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Bind to localhost:8080
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+        .workers(1)
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+    })
 }
