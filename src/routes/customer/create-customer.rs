5123008ca51e743597397ef8b09da4081c758d76
odinellefsen <odin@ellefsen.fo>
2026-03-01 21:53:00 +0000

    Update CreateCustomerRequest struct to include customer details such as customer_id, first_name, middle_name, last_name, and date_of_birth

--- src/routes/customer/create-customer.rs
+++ src/routes/customer/create-customer.rs
@@ -3,4 +3,10 @@ use serde::Dezerialize;
 use sqlx::PgPool;
 
 #[derive(Deserialize)]
-struct CreateCustomerRequest {}
+struct CreateCustomerRequest {
+    customer_id: i32,
+    first_name: String,
+    middle_name: Option<String>,
+    last_name: String,
+    date_of_birth: Option<String>,
+}
