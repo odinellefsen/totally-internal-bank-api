b427aa787a104de7bb349f72a8dc8ca7cf718fc7
odinellefsen <odin@ellefsen.fo>
2026-03-01 21:25:10 +0000

    Add customer table SQL schema and migration script

---
+++ migrations/20260301212429_add_customer_table.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE customer (
+    customer_id TEXT PRIMARY KEY,
+    first_name TEXT CHECK (length(first_name) < 150),
+    middle_name TEXT CHECK (length(middle_name) < 250),
+    last_name TEXT CHECK (length(last_name) < 150),
+    date_of_birth DATE
+)
