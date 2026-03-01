4f6cfcfd20713d845d94a165f5e9e64eb53b25c2
odinellefsen <odin@ellefsen.fo>
2026-03-01 22:16:10 +0000

    Update customer table to match migrations

--- db/objects/tables/customer.sql
+++ db/objects/tables/customer.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customer (
-    customer_id TEXT PRIMARY KEY,
+    customer_id INTEGER PRIMARY KEY CONSTRAINT customer_id_p_tal CHECK (customer_id BETWEEN 100000000 AND 999999999),
     first_name TEXT CHECK (length(first_name) < 150),
     middle_name TEXT CHECK (length(middle_name) < 250),
     last_name TEXT CHECK (length(last_name) < 150),
