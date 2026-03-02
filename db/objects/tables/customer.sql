7ec42c57a567b90f577b4587af85ab386b614f06
odinellefsen <odin@ellefsen.fo>
2026-03-02 14:15:52 +0000

    Refactor customer table constraints to improve data integrity and enforce NOT NULL checks on first_name and date_of_birth

--- db/objects/tables/customer.sql
+++ db/objects/tables/customer.sql
@@ -1,7 +1,7 @@
 CREATE TABLE customer (
-    customer_id INTEGER PRIMARY KEY CONSTRAINT customer_id_p_tal CHECK (customer_id BETWEEN 100000000 AND 999999999),
-    first_name TEXT CHECK (length(first_name) < 150),
-    middle_name TEXT CHECK (length(middle_name) < 250),
-    last_name TEXT CHECK (length(last_name) < 150),
-    date_of_birth DATE
+    customer_id INTEGER PRIMARY KEY CONSTRAINT customer_id_must_be_9_digits_chk CHECK (customer_id BETWEEN 100000000 AND 999999999),
+    first_name TEXT NOT NULL CONSTRAINT customer_first_name_len_chk CHECK (length(first_name) < 150),
+    middle_name TEXT CONSTRAINT customer_middle_name_len_chk CHECK (length(middle_name) < 250),
+    last_name TEXT CONSTRAINT customer_last_name_len_chk CHECK (length(last_name) < 150),
+    date_of_birth DATE NOT NULL
 )
