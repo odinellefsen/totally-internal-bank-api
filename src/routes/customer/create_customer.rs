289bd290656cfba7691c23b5c31ac3a90e6d03b9
odinellefsen <odin@ellefsen.fo>
2026-03-03 11:49:22 +0000

    Change date_of_birth field in CreateCustomerRequest from Option<String> to String to enforce required input for customer creation.

--- src/routes/customer/create_customer.rs
+++ src/routes/customer/create_customer.rs
@@ -10,7 +10,7 @@ struct CreateCustomerRequest {
     first_name: String,
     middle_name: Option<String>,
     last_name: String,
-    date_of_birth: Option<String>,
+    date_of_birth: String,
 }
 
 async fn create_customer(
