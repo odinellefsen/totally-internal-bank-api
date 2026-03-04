cde385335ffa63a5b1a4ec75158d60918f0ea466
odinellefsen <odin@ellefsen.fo>
2026-03-04 11:07:16 +0000

    Refactor create_customer function to utilize a stored procedure for customer creation, enhancing data handling and response structure.

--- src/routes/customer/create_customer.rs
+++ src/routes/customer/create_customer.rs
@@ -28,9 +28,8 @@ async fn create_customer(
 ) -> impl Responder {
     let result = sqlx::query_as::<_, Customer>(
         r#"
-        INSERT INTO customer (customer_id, first_name, middle_name, last_name, date_of_birth)
-        VALUES ($1, NULLIF(BTRIM($2), ''), NULLIF(BTRIM($3), ''), NULLIF(BTRIM($4), ''), $5::date)
-        RETURNING customer_id, first_name, middle_name, last_name, date_of_birth::text AS date_of_birth
+        SELECT customer_id, first_name, middle_name, last_name, date_of_birth
+        FROM create_customer($1, $2, $3, $4, $5::date)
         "#,
     )
     .bind(payload.customer_id)
